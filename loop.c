0aefe69cfd6b20f47092cb0c6290e7a21a93d1e7
vishnu333777 <vishnuballayya1@gmail.com>
2025-08-29 03:01:51 +0000

    loops

---
+++ loop.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int i = 1;
+
+    // while(i<=10){
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+
+
+    //   while(i<=10){
+    //     printf("%d\n",i*5);
+    //     i++;
+    // }
+
+    //     while(i<=10){
+    //     printf("5 X %d = %d\n",i,i*5);
+    //     i++;
+    // }
+
+
+    // Do-While 
+
+        // do{
+        //     printf("%d\n",i*5);
+        //     i++;
+        // }while(i>=10);
+
+
+//     // For Loop 
+
+//     for(i=1;i<=10;i++){
+//         printf("%d\n",i*5);
+//     }
+}
---
+++ vishnuloop.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+
+//     while(i<=10){
+//     printf("%d\n",i);
+//     i++;
+//     }
+// }
+
+//Do while
+// do{
+//     printf("%d\n", i*5);
+//     i++;
+// }while(i>=10);
+
+// }
+
+//For Loop
+
+// for(i=1;i<=10;i++){
+//         printf("%d\n",i*5);
+// }
+// }
